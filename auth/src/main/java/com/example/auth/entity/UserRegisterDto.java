d835aac40f77c63dd5ebd1c344a01eedc8ceff3e
GraczykNatala <natka.graczyk@gmail.com>
2023-09-05 16:35:01 +0200

    add Validation to RegisterDTO

--- auth/src/main/java/com/example/auth/entity/UserRegisterDto.java
+++ auth/src/main/java/com/example/auth/entity/UserRegisterDto.java
@@ -1,15 +1,24 @@
 package com.example.auth.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
 
 @Getter
 @Setter
 @Builder
 public class UserRegisterDto {
+
+    @Length(min = 5, max = 50, message = "Login powinien mieć od 5 do 50 znaków")
     private String login;
+    @Email
     private String email;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Length(min = 8, max = 75, message = "Hasło powinno mieć od 8 do 75 znaków")
     private String password;
     private Role role;
 }
