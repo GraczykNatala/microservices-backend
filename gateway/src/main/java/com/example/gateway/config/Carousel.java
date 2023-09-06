3d121435524a83d54ef703b96b6eb880d042eabf
GraczykNatala <natka.graczyk@gmail.com>
2023-09-06 12:01:29 +0200

    authenticationFilter

---
+++ gateway/src/main/java/com/example/gateway/config/Carousel.java
@@ -0,0 +1,17 @@
+package com.example.gateway.config;
+
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class Carousel {
+
+
+
+    public String getUriAuth(){
+        return "uriAuth";
+    }
+
+
+}
