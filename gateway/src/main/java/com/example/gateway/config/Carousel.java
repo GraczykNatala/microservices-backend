83d8f4d7a363b97b61aa370257eb499e9cf52e96
GraczykNatala <natka.graczyk@gmail.com>
2023-09-06 13:31:30 +0200

    gateway and auth configuration

--- gateway/src/main/java/com/example/gateway/config/Carousel.java
+++ gateway/src/main/java/com/example/gateway/config/Carousel.java
@@ -1,17 +1,50 @@
 package com.example.gateway.config;
 
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
 public class Carousel {
 
+    private final EurekaClient eurekaClient;
+    List<InstanceInfo> instances = new ArrayList<>();
+    int currentIndex = 0;
 
+    public Carousel(EurekaClient eurekaClient) {
+        this.eurekaClient = eurekaClient;
+        initAuthCarousel();
+        events();
+    }
+    private void initAuthCarousel() {
+        instances = eurekaClient.getApplication("AUTH-SERVICE").getInstances();
+    }
+    private void events(){
+        eurekaClient.registerEventListener(eurekaEvent -> {
+            initAuthCarousel();
+        });
+        eurekaClient.unregisterEventListener(eurekaEvent -> {
+            initAuthCarousel();
+        });
+    }
 
     public String getUriAuth(){
-        return "uriAuth";
+        StringBuilder stringBuilder = new StringBuilder();
+        InstanceInfo instance = instances.get(currentIndex);
+        stringBuilder.append(instance.getIPAddr()).append(":").append(instance.getPort());
+        if(instances.size()-1 == currentIndex) {
+            currentIndex = 0;
+        } else {
+            currentIndex++;
+        }
+        return stringBuilder.toString();
     }
+
 
 
 }
