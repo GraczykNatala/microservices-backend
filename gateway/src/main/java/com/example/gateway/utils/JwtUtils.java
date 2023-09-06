c6cf8946c6c6752d8ef89fe26e9bacd0a5f7eea8
GraczykNatala <natka.graczyk@gmail.com>
2023-09-06 11:33:31 +0200

    create RouteValidator

--- gateway/src/main/java/com/example/gateway/utils/JwtUtils.java
+++ gateway/src/main/java/com/example/gateway/utils/JwtUtils.java
@@ -3,6 +3,7 @@ package com.example.gateway.utils;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -10,7 +11,10 @@ import java.security.Key;
 public class JwtUtils {
 
 
-public static final String SECRET = "2c8c2e88030791c05184ef724749983de9a7a90785a32f6b0d8e42acd09a7601";
+    public JwtUtils(@Value("${jwt.secret}") String secret){
+        SECRET = secret;
+    }
+    public final String SECRET;
 
 public void validateToken(final String token){
     Jwts.parserBuilder()
